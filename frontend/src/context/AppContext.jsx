5468e0db94d41f382f2f213cd2a88abbbd78176f
Kajol Roy <162959463+kajolroy24@users.noreply.github.com>
2025-04-24 21:22:31 +0530

    Update AppContext for deployment

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContext = createContext();
 const AppContextProvider = (props) => {
 
   const navigate = useNavigate();
-  const backendUrl = import.meta.env.VITE_BACKEND_URL
+  const backendUrl = "https://budget-flow-backend.onrender.com"
 
   const [darkMode, setDarkMode] = useState(() => {
     // Check localStorage for user's dark mode preference
